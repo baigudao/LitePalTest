af9ceaf2bd4ef8aa9c4b235671cef49ee34d6ae1
jackie <971060378@qq.com>
2016-12-21 05:22:00 +0800

    准备工作做完。

---
+++ app/src/main/java/com/jackie/litepaltest/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jackie.litepaltest.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jackie.litepaltest.R;
+
+/**
+ * Created by Administrator on 2016/12/21.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
